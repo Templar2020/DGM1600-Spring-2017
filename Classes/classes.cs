6540bb9c17ec86799c3ff7da19175de8993032b6
Preston Powell <preston.powell2020@gmail.com>
2017-02-02 18:17:59 -0700

    Exercise on Classes :)

---
+++ Classes/classes.cs
@@ -0,0 +1,14 @@
+// Class Example
+
+public class classes: MonoBehavior
+{
+ int score = 0;
+
+ public class cats {
+     public int shortHair = 1;
+     public int longHair = 2;
+     public int tabby = 1;
+     public int angryCat = 10000000;
+ }
+
+}
